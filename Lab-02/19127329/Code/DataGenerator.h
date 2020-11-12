d5889aff7bbb2eab1369f705cb815799158d09f0
Lê Hoàng Anh <lehoanganh.le2001@gmail.com>
2020-11-12 17:18:50 +0700

    1

---
+++ Lab-02/19127329/Code/DataGenerator.h
@@ -0,0 +1,35 @@
+#ifndef DATA_GENERATOR_H_
+#define DATA_GENERATOR_H_
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+
+template <class T>
+void HoanVi(T &a, T &b)
+{
+	T x = a;
+	a = b;
+	b = x;
+}
+
+//-------------------------------------------------
+
+// Hàm phát sinh mảng dữ liệu ngẫu nhiên
+void GenerateRandomData(int a[], int n);
+
+// Hàm phát sinh mảng dữ liệu có thứ tự tăng dần
+void GenerateSortedData(int a[], int n);
+
+// Hàm phát sinh mảng dữ liệu có thứ tự ngược (giảm dần)
+void GenerateReverseData(int a[], int n);
+
+// Hàm phát sinh mảng dữ liệu gần như có thứ tự
+void GenerateNearlySortedData(int a[], int n);
+
+void GenerateData(int a[], int n, int dataType);
+
+#endif
---
+++ Lab-02/19127329/Code/test.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include "Sort.h"
+#include "DataGenerator.h"
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+enum DataOrder { RANDOM, SORTED, REVERSE, NEARLY };
+enum SortAlgorithms { SELECTION, INSERTION, BUBBLE, HEAP, MERGE, QUICK, RADIX };
+
+double sortAndMeasureTime(int* &a, int n, int type) {
+    double runTime = -1;
+    clock_t start;
+
+    switch (type) {
+    case SELECTION:
+        start = clock();
+        SelectionSort(a, n);
+        runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+        break;
+    case INSERTION:
+        start = clock();
+        InsertionSort(a, n);
+        runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+        break;
+    case BUBBLE:
+        start = clock();
+        BubbleSort(a, n);
+        runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+        break;
+    // case HEAP:
+    //     start = clock();
+    //     HeapSort(a, n);
+    //     runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+    //     break;
+    // case MERGE:
+    //     start = clock();
+    //     MergeSort(a, n);
+    //     runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+    //     break;
+    // case QUICK:
+    //     start = clock();
+    //     QuickSort(a, n);
+    //     runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+    //     break;
+    // case RADIX:
+    //     start = clock();
+    //     RadixSort(a, n);
+    //     runTime = (double) (clock() - start) / CLOCKS_PER_SEC;
+    //     break;
+    default:
+        break;
+    }
+    
+    return runTime;
+}
+
+int main() {
+    vector<int> dataSize { 3000, 10000, 30000, 100000, 300000 };
+    vector<string> dataOrder { "Random", "Sorted", "Reverse", "Nearly" };
+    vector<string> algorithms { 
+        "Selection Sort"};/*, "Insertion Sort", "Bubble Sort",
+        "Heap Sort", "Merge Sort", "Quick Sort", "Radix Sort" 
+    };*/
+
+    int* a = nullptr;
+
+    for (int i = 0; i < dataOrder.size(); i++) {
+        for (int j = 0; j < dataSize.size(); j++) {
+            for (int k = 0; k < algorithms.size(); k++) {
+                a = new int[dataSize[j]];
+
+                if (a == nullptr) {
+                    cout << "Error: not enough memory." << endl;
+                    j = dataSize.size();
+                    i = dataOrder.size();
+                    break;
+                }
+
+                GenerateData(a, dataSize[j], i);
+
+                double runTime = sortAndMeasureTime(a, dataSize[j], k);
+                cout << "----------------------------------------" << endl;
+                cout << "- Algorithm : " << algorithms[k] << endl;
+                cout << "- Data Size : " << dataSize[j] << endl;
+                cout << "- Data Order: " << dataOrder[i] << endl;
+                cout << "- Time      : " << fixed << setprecision(9) << runTime << "ms" << endl;
+                cout << "----------------------------------------" << endl;
+                
+                delete[] a;
+            }
+        }
+    }
+
+    return 0;
+}
